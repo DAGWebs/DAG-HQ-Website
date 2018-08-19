90ede7afcf8983449ffa0ef94b00c780a299f47f
DAG <kaseyber12@gmail.com>
2018-08-18 22:46:37 -0700

    Started Git for DAG
    Started the get repository for DAG HQ

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>DAG | </title>
+	<script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Baloo|Black+Ops+One|Bungee+Inline|Bungee+Shade|Corben|Diplomata+SC|Radley" rel="stylesheet">
+</head>
+<body>
+	<nav class="main-menu">
+		<label for="menu-toggler"><i class="fas fa-bars"></i></label>
+		<h2 class="logo">Dark Arts Gaming</h2>
+		<input type="checkbox" id="menu-toggler">
+		<ul id="mainNavUl">
+			<li class="hover"><a href="#" class="hover">Home</a></li>
+			<li>
+				<a href="#">Support <i class="fas fa-caret-down"></i></a>
+				<ul>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Tickets</a></li>
+				</ul>
+			</li>
+			<li class="hover"><a href="#" class="hover">Forums</a></li>
+			<li>
+				<a href="#">Code of Conduct <i class="fas fa-caret-down"></i></a>
+				<ul>
+					<li><a href="#">Privacy Policy</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="#">Ranks <i class="fas fa-caret-down"></i></a>
+				<ul>
+					<li><a href="#">Clan Ranks</a></li>
+					<li><a href="#">Discord Ranks</a></li>
+					<li><a href="#">Staff Ranks</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="#"><i class="fas fa-door-closed"></i> Login</a>
+				<ul>
+					<li><a href="#"><i class="fas fa-users"></i> Register</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+	<section id="wrapper" class="skewed">
+		<div class="layer bottom">
+			<div class="content-wrap">
+				<div class="content-body">
+					<h1>H1 TEXT</h1>
+					<p></p>
+				</div>
+			</div>
+			<img src="img/img2.png" alt="">
+		</div>
+		<div class="layer top">
+			<div class="content-wrap">
+				<div class="content-body">
+					<h1>H1 Text 2</h1>
+					<p></p>
+				</div>
+			</div>
+			<img src="img/img1.png" alt="">
+		</div>
+		<dov class="handle"></dov>
+	</section>
+	<div class="wrapper">
+		<div class="row">
+			<div class="col-4">
+				
+			</div>
+			<div class="col-8">
+				
+			</div>
+		</div>
+	</div>
+	<script src="js/main.js"></script>
+	<scriptsrc="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+</body>
+</html>
